5257cf5294b174e5b339c45e14a1efcc1f47e8ae
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 00:45:45 +0530

    json response

--- myApp.js
+++ myApp.js
@@ -8,7 +8,12 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 app.use('/public',express.static(__dirname+"/public"))
+app.get("/json",(req,res)=>{
+    res.json({message : "Hello json"})
+})
 console.log("Hello World")
+
+
 
 
 
