c7b6f07d1d1e4beb5de8ada3bf38f5f7bf178f01
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 00:23:47 +0530

    File

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.get("/",(req,res)=>{
-    res.send("Hello Express")
+    // res.send("Hello Express")
+    res.sendFile(__dirname+"/views/index.html")
 })
 console.log("Hello World")
 
