9998f5a3a019ad579782f48a407126de0767819c
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 02:31:24 +0530

    time

--- myApp.js
+++ myApp.js
@@ -11,6 +11,12 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 app.use('/public',express.static(__dirname+"/public"))
+app.get('/now',(req,res,next)=>{
+    req.time=new Date().toString();
+    next()
+},(req,res)=>{
+    res.json({time:req.time})
+})
 
 
 app.get("/json",(req,res)=>{
