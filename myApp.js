5bcb18739e21d53893d1a43927ec5362e097589a
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 02:27:01 +0530

    Middleware for logger

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 
+const loggerMiddleware=(req,res,next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+}
+app.use(loggerMiddleware);
 app.get("/",(req,res)=>{
     // res.send("Hello Express")
     res.sendFile(__dirname+"/views/index.html")
 })
 app.use('/public',express.static(__dirname+"/public"))
+
 
 app.get("/json",(req,res)=>{
     let message = "Hello json";
