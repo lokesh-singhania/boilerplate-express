eae20177ba11aa04b93438b3fcf98f1c7dd4f6ab
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 02:53:57 +0530

    post request

--- myApp.js
+++ myApp.js
@@ -23,7 +23,8 @@ app.route("/name").get((req,res)=>{
     const {first,last}=req.query;
     res.json({name:`${first} ${last}`})
 }).post((req,res)=>{
-
+    const {first,last}=req.body;
+    res.json({name:`${first} ${last}`})
 })
 
 app.get("/json",(req,res)=>{
