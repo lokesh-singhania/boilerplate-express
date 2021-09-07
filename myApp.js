d2128113aca14eeac62ad7950c000cd6d117ec64
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 00:36:22 +0530

    Assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+
+
+
 app.get("/",(req,res)=>{
     // res.send("Hello Express")
     res.sendFile(__dirname+"/views/index.html")
 })
+app.use('/public',express.static(__dirname+"/public"))
 console.log("Hello World")
 
 
