9b715f7de61773bf16ce8c21555622c40751fcec
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 01:12:46 +0530

    environment file

--- myApp.js
+++ myApp.js
@@ -8,8 +8,14 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html")
 })
 app.use('/public',express.static(__dirname+"/public"))
+const style=process.env.MESSAGE_STYLE;
+console.log("style"+process.env.MESSAGE_STYLE)
 app.get("/json",(req,res)=>{
-    res.json({message : "Hello json"})
+    let message = "Hello json";
+    if(process.env.MESSAGE_STYLE=="uppercase"){
+        message=message.toUpperCase();
+    }
+    res.json({message})
 })
 console.log("Hello World")
 
