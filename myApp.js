148064142a90ff671b37c05d774a28c3da256232
Lokesh Singhania <lokesh.singhania@gmail.com>
2021-09-08 01:19:39 +0530

    Removing comments

--- myApp.js
+++ myApp.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var app = express();
 
-
-
 app.get("/",(req,res)=>{
     // res.send("Hello Express")
     res.sendFile(__dirname+"/views/index.html")
 })
 app.use('/public',express.static(__dirname+"/public"))
 
-console.log("style"+process.env.MESSAGE_STYLE)
 app.get("/json",(req,res)=>{
     let message = "Hello json";
     if(process.env.MESSAGE_STYLE=="uppercase"){
